a08a0f31c363326303393e4000cdf803bd3b802c
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2018-01-23 13:08:45 +0530

    [TQE] Refactoring

--- summarizer/tqe/baseline.py
+++ summarizer/tqe/baseline.py
@@ -465,3 +465,40 @@ def _evaluate(y_pred, y_test, output=True):
         "pearsonR": pearsonR,
         "spearmanR": spearmanR
     }
+
+
+def setupArgparse(parser):
+    def run(args):
+        train_model(args.workspace_dir,
+                    args.model_name,
+                    devFileSuffix=args.dev_file_suffix,
+                    featureFileSuffix=args.feature_file_suffix,
+                    normalize=args.normalize,
+                    tune=args.tune,
+                    trainLM=args.train_lm,
+                    trainNGrams=args.train_ngrams,
+                    parseSentences=args.parse,
+                    maxJobs=args.max_jobs)
+
+    parser.add_argument('workspace_dir',
+                        help='Directory containing prepared files')
+    parser.add_argument('model_name',
+                        help='Identifier for prepared files used with ' +
+                        'preparation')
+    parser.add_argument('--dev-file-suffix', type=str, default=None,
+                        help='Suffix for test files')
+    parser.add_argument('--feature-file-suffix', type=str, default=None,
+                        help='Suffix for feature files')
+    parser.add_argument('--train-lm', action='store_true',
+                        help='Train language model.')
+    parser.add_argument('--train-ngrams', action='store_true',
+                        help='Compute ngram freqs.')
+    parser.add_argument('--parse', action='store_true',
+                        help='Parse sentences.')
+    parser.add_argument('--normalize', action='store_true',
+                        help='Weather to normalize features or not.')
+    parser.add_argument('--tune', action='store_true',
+                        help='Weather to tune parameters or not.')
+    parser.add_argument('--max-jobs', type=int, default=-1,
+                        help='Maximum number of jobs to run parallelly')
+    parser.set_defaults(func=run)
