3f59a897368447c77a17bab1f1ef496891693817
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2018-03-03 11:15:54 +0530

    Move Optimizer to linBilmes.py

--- summarizer/linBilmes.py
+++ summarizer/linBilmes.py
@@ -1,11 +1,62 @@
 from corpus import Corpus
-from optimizer import Optimizer
+from summary import Summary
 
 import objectives
 from objectives import AggregateObjective
 
 import logging
-logger = logging.getLogger("summarizer.py")
+logger = logging.getLogger("linBilmes.py")
+
+
+class Optimizer(object):
+    def __init__(self):
+        pass
+
+    def greedy(self, sizeBudget, objective, corpus):
+        summary = Summary()
+        sentencesLeft = corpus.getSentences()
+
+        objective.setCorpus(corpus)
+
+        sizeBudget, countTokens = sizeBudget
+        sizeName = "tokens" if countTokens else "chars"
+
+        def sentenceSize(sent):
+            return sent.tokenCount() if countTokens else sent.charCount()
+
+        def summarySize(summary):
+            return summary.tokenCount() if countTokens else summary.charCount()
+
+        logger.info("Greedily optimizing the objective")
+        logger.info("Summary budget: %d %s", sizeBudget, sizeName)
+        while summarySize(summary) < sizeBudget and len(sentencesLeft) > 0:
+            objectiveValues = map(objective.getObjective(summary),
+                                  sentencesLeft)
+            maxObjectiveValue = max(objectiveValues)
+
+            candidates = [sentencesLeft[i]
+                          for i, v in enumerate(objectiveValues)
+                          if v == maxObjectiveValue]
+
+            candidateSizes = map(sentenceSize, candidates)
+            minSize = min(candidateSizes)
+
+            selectedCandidate = candidates[candidateSizes.index(minSize)]
+            sentencesLeft.remove(selectedCandidate)
+
+            if summarySize(summary) + minSize <= sizeBudget:
+                logger.info("Sentence added with objective value: %f, " +
+                            "size: %d", maxObjectiveValue, minSize)
+                summary.addSentence(selectedCandidate)
+
+            budgetLeft = sizeBudget - summarySize(summary)
+            sentencesLeft = filter(lambda s: sentenceSize(s) < budgetLeft,
+                                   sentencesLeft)
+
+        logger.info("Optimization done, summary size: %d chars, %d tokens",
+                    summary.charCount(), summary.tokenCount())
+
+        return summary
 
 
 def summarize(inDir, params):
