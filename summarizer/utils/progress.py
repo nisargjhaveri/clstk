27f7260f70793367c2ea16533f7c5cb82675b2fd
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2018-05-15 12:13:40 +0530

    Update in progress bar util

--- summarizer/utils/progress.py
+++ summarizer/utils/progress.py
@@ -9,8 +9,12 @@ class ProgressBar(object):
         print ("----5---10---15---20---25---30---35---40---45---50" +
                "---55---60---65---70---75---80---85---90---95--100")
 
-    def _printProgress(self, progress):
+    def _printProgress(self, progress, newline=False):
         sys.stdout.write("*" * progress)
+
+        if newline:
+            sys.stdout.write("\n")
+
         sys.stdout.flush()
 
     def done(self, doneCount):
@@ -21,8 +25,5 @@ class ProgressBar(object):
         self._pastProgress = progressTo
 
     def complete(self):
-        self._printProgress(100 - self._pastProgress)
+        self._printProgress(100 - self._pastProgress, newline=True)
         self._pastProgress = 100
-
-        # Print newline at the end
-        print
